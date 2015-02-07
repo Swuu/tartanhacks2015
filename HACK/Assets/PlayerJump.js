03e9d6e174b05a4a15d43b1f5500984ed954bb69
wganucheau <ssx@andrew.cmu.edu>
2015-02-06 23:38:34 -0500

    Jump

---
+++ HACK/Assets/PlayerJump.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+private var airTime = 0.25;
+private var jumpSpeed = 3;
+private var startJumpTime = 0.0;
+private var jumping = false;
+
+function Update ()
+{
+	if (Input.GetKeyDown (KeyCode.UpArrow) && !jumping)
+	{
+		jumping = true;
+		startJumpTime = Time.time;
+	}
+	
+	if (jumping)
+	{
+		if (Time.time < startJumpTime + airTime)
+			transform.Translate (Vector3.up * Time.deltaTime * jumpSpeed);
+		else if (Time.time < startJumpTime + 2 * airTime)
+			transform.Translate (Vector3.down * Time.deltaTime * jumpSpeed);
+		else
+		{
+			jumping = false;
+		}
+	}
+}
